b4c9ab10c10f1242810d331d06d209e60fb3cc3a
oblerion <oblerion@protonmail.com>
2023-06-29 16:58:28 +0200

    fix print and add object

--- JsonObject.cpp
+++ JsonObject.cpp
@@ -141,7 +141,6 @@ JsonObject::~JsonObject()
     _das1.clear();
     _das1_keys.clear();
 }
-
 void JsonObject::_addArray(std::string name,std::string value)
 {
     std::string ext = _extract(value,'[',']');
@@ -406,8 +405,8 @@ std::string JsonObject::ToString()
         sdata = "[";
         for(unsigned int j=0;j<_dai1[i].size();j++)
         {
+            if(j!=0) sdata += ", ";
             sdata += TextFormat(" %d",_dai1[i][j]);
-            if(i<_dai1.size()) sdata += ", ";
         }
         sdata += "]";
         as_key.push_back(TextFormat("%c%s%c",'"',_dai1_keys[i].c_str(),'"'));
@@ -420,8 +419,8 @@ std::string JsonObject::ToString()
         sdata = "[";
         for(unsigned int j=0;j<_das1[i].size();j++)
         {
+            if(j!=0) sdata += ", ";
             sdata += '"' + _das1[i][j] + '"';
-            if(j<_das1[i].size()-1) sdata += ", ";
         }
         sdata += "]";
         as_key.push_back(TextFormat("%c%s%c",'"',_das1_keys[i].c_str(),'"'));
@@ -435,8 +434,9 @@ std::string JsonObject::ToString()
     std::string s="{";
     for(unsigned int i=0;i<as_key.size();i++)
     {
+        if(i!=0) s += ", ";
         s += "\n" + as_key[i] + " :" + as_value[i];
-        if(i<as_key.size()-1) s += ",";
+      
     }
     s += "\n}";
     //puts(s.c_str());
@@ -574,12 +574,12 @@ void JsonObject::SetObject(std::string key, JsonObject value)
     for(unsigned int i=0;i<_dj_keys.size();i++)
     {
         if(_dj_keys[i]==key){
-            _dj[i] = *(new JsonObject(value.ToString()));
+            _dj[i] = value;//*(new JsonObject(value.ToString()));
             return;
         }
     }
 	_dj_keys.push_back(key);
-	_dj.push_back( *(new JsonObject(value.ToString())));
+	_dj.push_back(value);//*(new JsonObject(value.ToString())));
 
 }
 void JsonObject::SetBool(std::string key, bool value)
